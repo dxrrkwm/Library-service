61cf4c2b7c743a89b42f4dead7ff5893a1def6ac
roman <skyfoxwork@gmail.com>
2025-01-03 16:59:19 +0200

    fix N+1 problem in borrowings/views.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -2,14 +2,14 @@ from rest_framework import generics
 from .models import Borrowing
 from .serializers import (
     BorrowingDetailSerializer,
-    BorrowingListSerializer,
+    BorrowingSerializer,
 )
 
 
 class BorrowingListView(generics.ListCreateAPIView):
-    queryset = Borrowing.objects.all()
-    serializer_class = BorrowingListSerializer
+    queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingSerializer
 
 class BorrowingDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Borrowing.objects.all()
+    queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingDetailSerializer
