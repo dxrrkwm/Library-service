db290eaf03e444444283af378c5e9bf8e34a9803
roman <skyfoxwork@gmail.com>
2025-01-03 23:47:03 +0200

    refactor Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,9 +1,5 @@
-from datetime import datetime
-from decimal import Decimal
-
 from django.conf import settings
 from django.db import models
-from rest_framework.exceptions import ValidationError
 
 from books_service.models import Book
 
@@ -23,32 +19,5 @@ class Borrowing(models.Model):
         related_name="borrowings"
     )
 
-    def calculate_total_price(self):
-        if self.actual_return_date:
-            days_borrowed = (self.actual_return_date - self.borrow_date).days
-        else:
-            days_borrowed = (datetime.date.today() - self.borrow_date).days
-        return Decimal(days_borrowed) * self.book.daily_fee
-
-    def clean(self):
-        if self.borrow_date > datetime.date.today():
-            raise ValidationError(
-                {"borrow_date": "The borrow date cannot be in the future."}
-            )
-
-        if self.expected_return_date <= self.borrow_date:
-            raise ValidationError(
-                {"expected_return_date": "The expected return date must be after the borrow date."}
-            )
-
-
-        if self.actual_return_date and self.actual_return_date < self.borrow_date:
-            raise ValidationError(
-                {"actual_return_date": "The actual return date cannot be earlier than the borrow date."}
-            )
-
-
-        if self.actual_return_date and self.actual_return_date < self.expected_return_date:
-            raise ValidationError(
-                {"actual_return_date": "The actual return date cannot be earlier than the expected date."}
-            )
+    def __str__(self) -> str:
+        return f"Borrowing: {self.book.title} by {self.user.email}"
