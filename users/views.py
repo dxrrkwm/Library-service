9cb217816e4c4b6061ad84033199f6b537b71d15
mregorlite <egor.infinity@gmail.com>
2025-01-03 10:12:22 +0200

    fix django rest package

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django_rest.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
