be48230f2de55574ac4a866da8d3a68b8205ba83
roman <skyfoxwork@gmail.com>
2025-01-03 23:47:46 +0200

    refactor serializers

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,26 +1,24 @@
 from rest_framework import serializers
 
-from users.serializers import UserSerializer
-from .models import Borrowing
 from books_service.serializers import BookSerializer
+from users.serializers import UserSerializer
+
+from .models import Borrowing
+
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Borrowing
-        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user")
-
-
-class BorrowingListSerializer(serializers.ModelSerializer):
-    book = serializers.CharField(
-        source="book.title", read_only=True
-    )
-    user = serializers.CharField(
-        source="user.email", read_only=True
-    )
+    actual_return_date = serializers.DateField(read_only=True)
 
     class Meta:
         model = Borrowing
-        fields = ("id",  "book", "user")
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user"
+        )
 
 
 class BorrowingDetailSerializer(BorrowingSerializer):
