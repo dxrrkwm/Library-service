3af4666af14e565409042bf9689ebbe15fe8b523
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-01-03 08:17:39 +0200

    Add JWT authentication and IsAdminOrReadOnly permissions

--- books_service/views.py
+++ books_service/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books_service.models import Book
+from books_service.permissions import IsAdminOrReadOnly
 from books_service.serializers import (
     BookDetailSerializer,
     BookListSerializer,
@@ -10,6 +12,8 @@ from books_service.serializers import (
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
+    authentication_classes = [JWTAuthentication]
 
     action_serializer_classes = {
         "list": BookListSerializer,
