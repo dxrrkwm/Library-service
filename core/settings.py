655c9cb5af4bbe074b08d96c129730de8ffb507d
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-01-03 09:19:53 +0200

    Add default value for SECRET_KEY in case environment variable is not set

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY", "*u$$uujq#m(0#%1ub%dmue##x93s&pgv9!&e1%b^ghw8mk%ye@")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
