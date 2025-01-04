1f6d306fb03dc04848db16f9a4cb770ed7196886
roman <skyfoxwork@gmail.com>
2025-01-04 14:32:19 +0200

    fix plural form

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from borrowings.views import BorrowingDetailView, BorrowingListView, ReturnBookView
 
 urlpatterns = [
-    path("", BorrowingListView.as_view(), name="borrowing-list"),
+    path("", BorrowingListView.as_view(), name="borrowings-list"),
     path("<int:pk>/", BorrowingDetailView.as_view(), name="borrowing-detail"),
     path("<int:pk>/return/", ReturnBookView.as_view(), name="return-book"),
 ]
