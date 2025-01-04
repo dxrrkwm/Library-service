67765c25e9ae37e4d2f7f6e2b397a1177d062c0b
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-01-04 21:01:44 +0200

    Implement Payment views with list and detail endpoints, with access control for non-admin users

--- payments/views.py
+++ payments/views.py
@@ -1,2 +1,31 @@
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import (
+    PaymentDetailSerializer,
+    PaymentListSerializer,
+    PaymentSerializer,
+)
+
+
+class PaymentListRetrieveViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin
+):
+    permission_classes = [IsAuthenticated]
+    action_serializer_classes = {
+        "list": PaymentListSerializer,
+        "retrieve": PaymentDetailSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.action_serializer_classes.get(self.action, PaymentSerializer)
+
+    def get_queryset(self):
+        queryset = Payment.objects.select_related("borrowing")
+
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(borrowing__user=self.request.user)
