7e6c41e1fd890fb322b2eb7c1c27b8bda21bf753
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-01-05 12:04:17 +0200

    Revert ACCESS_TOKEN_LIFETIME to 30 minutes after testing

--- core/settings/base.py
+++ core/settings/base.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
 
 # JWT settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
     "AUTH_HEADER_TYPES": ("Bearer",),
