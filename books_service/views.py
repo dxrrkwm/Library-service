bc0a08921edbc905f263940418f17abba6531571
roman <skyfoxwork@gmail.com>
2025-01-05 14:41:43 +0200

    add upload_image action BookViewSet to book_service/views.py

--- books_service/views.py
+++ books_service/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books_service.models import Book
@@ -22,6 +25,22 @@ class BookViewSet(viewsets.ModelViewSet):
         "update": BookSerializer,
         "partial_update": BookSerializer
     }
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        movie = self.get_object()
+        serializer = self.get_serializer(movie, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self):
         return self.action_serializer_classes.get(self.action, BookSerializer)
