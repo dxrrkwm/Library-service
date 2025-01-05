3846d33be7758e9cb56a654b82d14f592481b598
Olha Stadnik <olgastadnik2507@gmail.comcd>
2025-01-05 23:43:31 +0200

    fix ruff mistake in admin for book and borrowing

--- books_service/admin.py
+++ books_service/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Book
 
 
@@ -9,4 +8,3 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("title", "author")
     list_filter = ("author",)
     ordering = ("-author",)
-
--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from borrowings.models import Borrowing
 
 @admin.register(Borrowing)
