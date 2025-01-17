23a4539de59133274dd7f79c4c73c6cc4cf9baf0
roman <skyfoxwork@gmail.com>
2025-01-17 17:13:28 +0200

    fix read_only_fields in borrowings/serializers.py

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -16,7 +16,6 @@ class ShortInfoPaymentSerializer(serializers.ModelSerializer):
             "status",
             "type"
         )
-        read_only_fields = fields
 
 
 class DetailInfoPaymentSerializer(serializers.ModelSerializer):
@@ -29,7 +28,6 @@ class DetailInfoPaymentSerializer(serializers.ModelSerializer):
             "money_to_pay",
             "session_url"
         )
-        read_only_fields = fields
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
